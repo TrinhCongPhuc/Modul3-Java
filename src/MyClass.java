2125c408a9886775965b2156581b20d62a1a17d7
TrinhCongPhuc <130523140+TrinhCongPhuc@users.noreply.github.com>
2023-09-20 09:12:20 +0700

    Add files via upload

---
+++ src/MyClass.java
@@ -0,0 +1,28 @@
+public class MyClass {
+    private String myString;
+
+    public MyClass() {
+    }
+
+    public MyClass(String myString) {
+        this.myString = myString;
+    }
+
+    public static void main(String[] args) {
+        MyClass myObject = new MyClass();
+
+        System.out.println("Giá trị ban đầu của myString: " + myObject.getMyString());
+
+        myObject.setMyString("Xin Chào");
+
+        System.out.println("Giá trị mới của myString: " + myObject.getMyString());
+    }
+
+    public String getMyString() {
+        return myString;
+    }
+
+    public void setMyString(String myString) {
+        this.myString = myString;
+    }
+}
